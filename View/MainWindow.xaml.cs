5a2391e066b8cbf8088cf217ed1f9fa5de3e40af
viktor <vkt.knnnk@gmail.com>
2024-01-20 23:13:21 +0500

    added DataContext to MainWindow

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EnigmaClientV3.View_Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,7 @@ namespace EnigmaClientV3.View
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainWindowVM();
         }
     }
 }
