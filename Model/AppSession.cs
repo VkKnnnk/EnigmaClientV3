9c800c362e6631213101743642c678c05cd1dada
viktor <vkt.knnnk@gmail.com>
2024-01-20 20:32:13 +0500

    updated

---
+++ Model/AppSession.cs
@@ -0,0 +1,23 @@
+﻿using EnigmaClientV3.Model.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnigmaClientV3.Model
+{
+    public static class AppSession
+    {
+        private static compclub_dbContext _context;
+        public static compclub_dbContext Context
+        {
+            get
+            {
+                if (_context is null)
+                    _context = new compclub_dbContext();
+                return _context;
+            }
+        }
+    }
+}
