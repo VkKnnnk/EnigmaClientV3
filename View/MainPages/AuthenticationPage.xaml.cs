e59a178321f7101ac8d21eb4915e666b8065dac0
viktor <vkt.knnnk@gmail.com>
2024-01-20 20:29:12 +0500

    added DataContext

--- View/MainPages/AuthenticationPage.xaml.cs
+++ View/MainPages/AuthenticationPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EnigmaClientV3.View_Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace EnigmaClientV3.View.MainPages
         public AuthenticationPage()
         {
             InitializeComponent();
+            DataContext = new AuthenticationPageVM();
         }
     }
 }
